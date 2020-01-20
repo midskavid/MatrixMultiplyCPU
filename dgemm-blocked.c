f0ea323702b1c0c758e5f7a33fa28befabe55bce
midskavid <midskavid@yahoo.co.in>
2020-01-19 18:12:32 -0800

    Adding changes for level blocking

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -8,10 +8,18 @@
 
 const char* dgemm_desc = "Simple blocked dgemm.";
 
-#if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 37
-// #define BLOCK_SIZE 719
+#if !defined(BLOCK_SIZEL1)
+#define BLOCK_SIZEL1 37
 #endif
+
+#if !defined(BLOCK_SIZEL2)
+#define BLOCK_SIZEL2 120
+#endif
+
+#if !defined(BLOCK_SIZEL3)
+#define BLOCK_SIZEL3 750
+#endif
+
 
 #define min(a,b) (((a)<(b))?(a):(b))
 
@@ -37,6 +45,53 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
     }
 }
 
+static void do_blockL1 (int lda, int M, int N, int K, double* A, double* B, double* C)
+{
+  /* For each block-row of A */ 
+  for (int i = 0; i < M; i += BLOCK_SIZEL1)
+    /* For each block-column of B */
+    for (int j = 0; j < N; j += BLOCK_SIZEL1)
+      /* Accumulate block dgemms into block of C */
+      for (int k = 0; k < K; k += BLOCK_SIZEL1)
+      {
+  /* Correct block dimensions if block "goes off edge of" the matrix */
+  int M_ = min (BLOCK_SIZEL1, M-i);
+  int N_ = min (BLOCK_SIZEL1, N-j);
+  int K_ = min (BLOCK_SIZEL1, K-k);
+
+  /* Perform individual block dgemm */
+#ifdef TRANSPOSE
+  do_block(lda, M_, N_, K_, A + i*lda + k, B + j*lda + k, C + i*lda + j);
+#else
+  do_block(lda, M_, N_, K_, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+#endif
+      }
+}
+
+static void do_blockL2 (int lda, int M, int N, int K, double* A, double* B, double* C)
+{
+  /* For each block-row of A */ 
+  for (int i = 0; i < M; i += BLOCK_SIZEL2)
+    /* For each block-column of B */
+    for (int j = 0; j < N; j += BLOCK_SIZEL2)
+      /* Accumulate block dgemms into block of C */
+      for (int k = 0; k < K; k += BLOCK_SIZEL2)
+      {
+  /* Correct block dimensions if block "goes off edge of" the matrix */
+  int M_ = min (BLOCK_SIZEL2, M-i);
+  int N_ = min (BLOCK_SIZEL2, N-j);
+  int K_ = min (BLOCK_SIZEL2, K-k);
+
+  /* Perform individual block dgemm */
+#ifdef TRANSPOSE
+  do_blockL1(lda, M_, N_, K_, A + i*lda + k, B + j*lda + k, C + i*lda + j);
+#else
+  do_blockL1(lda, M_, N_, K_, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+#endif
+      }
+}
+
+
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in row-major order
@@ -52,22 +107,22 @@ void square_dgemm (int lda, double* A, double* B, double* C)
     }
 #endif
   /* For each block-row of A */ 
-  for (int i = 0; i < lda; i += BLOCK_SIZE)
+  for (int i = 0; i < lda; i += BLOCK_SIZEL3)
     /* For each block-column of B */
-    for (int j = 0; j < lda; j += BLOCK_SIZE)
+    for (int j = 0; j < lda; j += BLOCK_SIZEL3)
       /* Accumulate block dgemms into block of C */
-      for (int k = 0; k < lda; k += BLOCK_SIZE)
+      for (int k = 0; k < lda; k += BLOCK_SIZEL3)
       {
 	/* Correct block dimensions if block "goes off edge of" the matrix */
-	int M = min (BLOCK_SIZE, lda-i);
-	int N = min (BLOCK_SIZE, lda-j);
-	int K = min (BLOCK_SIZE, lda-k);
+	int M = min (BLOCK_SIZEL3, lda-i);
+	int N = min (BLOCK_SIZEL3, lda-j);
+	int K = min (BLOCK_SIZEL3, lda-k);
 
 	/* Perform individual block dgemm */
 #ifdef TRANSPOSE
-	do_block(lda, M, N, K, A + i*lda + k, B + j*lda + k, C + i*lda + j);
+	do_blockL2(lda, M, N, K, A + i*lda + k, B + j*lda + k, C + i*lda + j);
 #else
-	do_block(lda, M, N, K, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+	do_blockL2(lda, M, N, K, A + i*lda + k, B + k*lda + j, C + i*lda + j);
 #endif
       }
 #if TRANSPOSE
