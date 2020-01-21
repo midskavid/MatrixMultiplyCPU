46fb38376d04fa4e05d8ef4f5f0818f57f78447d
midskavid <midskavid@yahoo.co.in>
2020-01-21 00:04:14 -0800

    SIMD changes break!

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -5,24 +5,46 @@
  *    Enable user to select one problem size only via the -n option
  *    Support CBLAS interface
  */
+#include <immintrin.h>
+#include <avx2intrin.h>
+#define min(a,b) (((a)<(b))?(a):(b))
 
-const char* dgemm_desc = "Simple blocked dgemm.";
+
 
 #if !defined(BLOCK_SIZEL2)
-#define BLOCK_SIZEL2 8
+#define BLOCK_SIZEL2 30
 #endif
 
 #if !defined(BLOCK_SIZEL3)
-#define BLOCK_SIZEL3 50
+#define BLOCK_SIZEL3 400
 #endif
 
 
-#define min(a,b) (((a)<(b))?(a):(b))
+const char* dgemm_desc = "Simple blocked dgemm.";
 
 /* This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
+ *  C := C + A * B using SIMD operations
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
+static inline void do_block_SIMD(int lda, int M, int N, int K, double* A, double* B, double* C) {
+  register __m256d c00_c01_c02_c03 = _mm256_loadu_pd(C);
+  register __m256d c10_c11_c12_c13 = _mm256_loadu_pd(C+lda);
+
+  for (int kk=0;kk<4;++kk) {
+    register __m256d a0x = _mm256_broadcast_sd(A+kk);
+    register __m256d a1x = _mm256_broadcast_sd(A+kk+lda);
+
+    register __m256d b = _mm256_loadu_pd(B+kk*lda);
+
+    c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
+    c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
+  }
+
+  _mm256_storeu_pd (C, c00_c01_c02_c03);
+  _mm256_storeu_pd (C+lda, c10_c11_c12_c13);
+}
+
+
+static inline void do_block_naive (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
   /* For each row i of A */
   for (int i = 0; i < M; ++i)
@@ -33,16 +55,52 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
       double cij = C[i*lda+j];
       for (int k = 0; k < K; ++k)
 #ifdef TRANSPOSE
-	cij += A[i*lda+k] * B[j*lda+k];
+  cij += A[i*lda+k] * B[j*lda+k];
 #else
-	cij += A[i*lda+k] * B[k*lda+j];
+  cij += A[i*lda+k] * B[k*lda+j];
 #endif
       C[i*lda+j] = cij;
     }
 }
 
+/* This auxiliary subroutine performs a smaller dgemm operation
+ *  C := C + A * B
+ * where C is M-by-N, A is M-by-K, and B is K-by-N. */
+static inline void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
+{
+  /* For each row i of A */
+  for (int i = 0; i < M; i+= 2)
+    /* For each column j of B */ 
+    for (int j = 0; j < N; j+= 4) 
+    {
+      /* Compute C(i,j) */
+      for (int k = 0; k < K; k+=4)
+      {
+        int M_ = min (2, M-i);
+        int N_ = min (4, N-j);
+        int K_ = min (4, K-k);
+        if (M_==2&&N_==4) 
+        {
+#ifdef TRANSPOSE
+  do_block_SIMD(lda, M_, N_, K_, A + i*lda + k, B + j*lda + k, C + i*lda + j);
+#else
+  do_block_SIMD(lda, M_, N_, K_, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+#endif   
+        }
+        else
+        {
+#ifdef TRANSPOSE
+  do_block_naive(lda, M_, N_, K_, A + i*lda + k, B + j*lda + k, C + i*lda + j);
+#else
+  do_block_naive(lda, M_, N_, K_, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+#endif
+        }
+      }
+    }
+}
 
-static void do_blockL2 (int lda, int M, int N, int K, double* A, double* B, double* C)
+
+static inline void do_blockL2 (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
   /* For each block-row of A */ 
   for (int i = 0; i < M; i += BLOCK_SIZEL2)
