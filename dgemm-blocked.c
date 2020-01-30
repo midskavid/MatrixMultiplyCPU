11465a53d769273693570d26f4bdab298b4c51e2
Shivam Lakhotia <shivamdlakhotia@gmail.com>
2020-01-30 12:38:43 +0000

    Current setting gives max 13.5 using L1 Caching only.

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -18,7 +18,7 @@ double *B_L2_CACHED = NULL;
 double *B_L1_CACHED = NULL;
 
 #if !defined(L1_M)
-#define L1_M 64
+#define L1_M 32
 #endif
 
 #if !defined(L1_N)
@@ -30,7 +30,7 @@ double *B_L1_CACHED = NULL;
 #endif
 
 #if !defined(L2_M)
-#define L2_M 64
+#define L2_M 32
 #endif
 
 #if !defined(L2_N)
@@ -38,11 +38,11 @@ double *B_L1_CACHED = NULL;
 #endif
 
 #if !defined(L2_K)
-#define L2_K 32
+#define L2_K 64
 #endif
 
 #if !defined(L3_M)
-#define L3_M 64
+#define L3_M 128
 #endif
 
 #if !defined(L3_N)
@@ -50,7 +50,7 @@ double *B_L1_CACHED = NULL;
 #endif
 
 #if !defined(L3_K)
-#define L3_K 64
+#define L3_K 128
 #endif
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
@@ -121,38 +121,34 @@ static inline void do_block_SIMD5x4(int lda, int ldb, int ldc, double *A, double
   _mm256_storeu_pd(C + 4 * ldc, c40_c41_c42_c43);
 }
 
-
-static inline void do_block_SIMD8x4(int lda, int ldb, int ldc, double* restrict A, double* restrict B, double* restrict C) {
+static inline void do_block_SIMD8x4(int lda, int ldb, int ldc, double *restrict A, double *restrict B, double *restrict C)
+{
   // A = __builtin_assume_aligned (A, 8);
   // B = __builtin_assume_aligned (B, 8);
   // C = __builtin_assume_aligned (C, 8);
 
   register __m256d c00_c01_c02_c03 = _mm256_loadu_pd(C);
-  register __m256d c10_c11_c12_c13 = _mm256_loadu_pd(C+ldc);
-  register __m256d c20_c21_c22_c23 = _mm256_loadu_pd(C+2*ldc);
-  register __m256d c30_c31_c32_c33 = _mm256_loadu_pd(C+3*ldc);
-  register __m256d c40_c41_c42_c43 = _mm256_loadu_pd(C+4*ldc);
-  register __m256d c50_c51_c52_c53 = _mm256_loadu_pd(C+5*ldc);
-  register __m256d c60_c61_c62_c63 = _mm256_loadu_pd(C+6*ldc);
-  register __m256d c70_c71_c72_c73 = _mm256_loadu_pd(C+7*ldc);
-  
-
+  register __m256d c10_c11_c12_c13 = _mm256_loadu_pd(C + ldc);
+  register __m256d c20_c21_c22_c23 = _mm256_loadu_pd(C + 2 * ldc);
+  register __m256d c30_c31_c32_c33 = _mm256_loadu_pd(C + 3 * ldc);
+  register __m256d c40_c41_c42_c43 = _mm256_loadu_pd(C + 4 * ldc);
+  register __m256d c50_c51_c52_c53 = _mm256_loadu_pd(C + 5 * ldc);
+  register __m256d c60_c61_c62_c63 = _mm256_loadu_pd(C + 6 * ldc);
+  register __m256d c70_c71_c72_c73 = _mm256_loadu_pd(C + 7 * ldc);
 
 #if 1
-  for (int kk=0;kk<4;++kk) {
-    register __m256d a0x = _mm256_broadcast_sd(A+kk);
-    register __m256d a1x = _mm256_broadcast_sd(A+kk+lda);
-    register __m256d a2x = _mm256_broadcast_sd(A+kk+2*lda);
-    register __m256d a3x = _mm256_broadcast_sd(A+kk+3*lda);
-    register __m256d a4x = _mm256_broadcast_sd(A+kk+4*lda);
-    register __m256d a5x = _mm256_broadcast_sd(A+kk+5*lda);
-    register __m256d a6x = _mm256_broadcast_sd(A+kk+6*lda);
-    register __m256d a7x = _mm256_broadcast_sd(A+kk+7*lda);
+  for (int kk = 0; kk < 4; ++kk)
+  {
+    register __m256d a0x = _mm256_broadcast_sd(A + kk);
+    register __m256d a1x = _mm256_broadcast_sd(A + kk + lda);
+    register __m256d a2x = _mm256_broadcast_sd(A + kk + 2 * lda);
+    register __m256d a3x = _mm256_broadcast_sd(A + kk + 3 * lda);
+    register __m256d a4x = _mm256_broadcast_sd(A + kk + 4 * lda);
+    register __m256d a5x = _mm256_broadcast_sd(A + kk + 5 * lda);
+    register __m256d a6x = _mm256_broadcast_sd(A + kk + 6 * lda);
+    register __m256d a7x = _mm256_broadcast_sd(A + kk + 7 * lda);
 
-
-
-    register __m256d b = _mm256_loadu_pd(B+kk*ldb);
-    
+    register __m256d b = _mm256_loadu_pd(B + kk * ldb);
 
     c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
     c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
@@ -162,88 +158,86 @@ static inline void do_block_SIMD8x4(int lda, int ldb, int ldc, double* restrict 
     c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
     c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
     c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
-
   }
 #else
-    register __m256d a0x = _mm256_broadcast_sd(A);
-    register __m256d a1x = _mm256_broadcast_sd(A+lda);
-    register __m256d a2x = _mm256_broadcast_sd(A+2*lda);
-    register __m256d a3x = _mm256_broadcast_sd(A+3*lda);
-    register __m256d a4x = _mm256_broadcast_sd(A+4*lda);
-    register __m256d a5x = _mm256_broadcast_sd(A+5*lda);
-    register __m256d a6x = _mm256_broadcast_sd(A+6*lda);
-    register __m256d a7x = _mm256_broadcast_sd(A+7*lda);
-    register __m256d b = _mm256_loadu_pd(B);
-    
-    c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
-    c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
-    c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
-    c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
-    c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b, c40_c41_c42_c43);
-    c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
-    c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
-    c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
-    a0x = _mm256_broadcast_sd(A+1);
-    a1x = _mm256_broadcast_sd(A+1+lda);
-    a2x = _mm256_broadcast_sd(A+1+2*lda);
-    a3x = _mm256_broadcast_sd(A+1+3*lda);
-    a4x = _mm256_broadcast_sd(A+1+4*lda);
-    a5x = _mm256_broadcast_sd(A+1+5*lda);
-    a6x = _mm256_broadcast_sd(A+1+6*lda);
-    a7x = _mm256_broadcast_sd(A+1+7*lda);
-    b = _mm256_loadu_pd(B+1*ldb);
-    c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
-    c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
-    c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
-    c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
-    c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b, c40_c41_c42_c43);
-    c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
-    c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
-    c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
-    a0x = _mm256_broadcast_sd(A+2);
-    a1x = _mm256_broadcast_sd(A+2+lda);
-    a2x = _mm256_broadcast_sd(A+2+2*lda);
-    a3x = _mm256_broadcast_sd(A+2+3*lda);
-    a4x = _mm256_broadcast_sd(A+2+4*lda);
-    a5x = _mm256_broadcast_sd(A+2+5*lda);
-    a6x = _mm256_broadcast_sd(A+2+6*lda);
-    a7x = _mm256_broadcast_sd(A+2+7*lda);
-    b = _mm256_loadu_pd(B+2*ldb);
-    c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
-    c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
-    c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
-    c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
-    c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b, c40_c41_c42_c43);
-    c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
-    c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
-    c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
-    a0x = _mm256_broadcast_sd(A+3);
-    a1x = _mm256_broadcast_sd(A+3+lda);
-    a2x = _mm256_broadcast_sd(A+3+2*lda);
-    a3x = _mm256_broadcast_sd(A+3+3*lda);
-    a4x = _mm256_broadcast_sd(A+3+4*lda);
-    a5x = _mm256_broadcast_sd(A+3+5*lda);
-    a6x = _mm256_broadcast_sd(A+3+6*lda);
-    a7x = _mm256_broadcast_sd(A+3+7*lda);
-    b = _mm256_loadu_pd(B+3*ldb);
-    c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
-    c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
-    c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
-    c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
-    c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b, c40_c41_c42_c43);
-    c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
-    c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
-    c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
-#endif
-  _mm256_storeu_pd (C, c00_c01_c02_c03);
-  _mm256_storeu_pd (C+ldc, c10_c11_c12_c13);
-  _mm256_storeu_pd (C+2*ldc, c20_c21_c22_c23);
-  _mm256_storeu_pd (C+3*ldc, c30_c31_c32_c33);
-  _mm256_storeu_pd (C+4*ldc, c40_c41_c42_c43);
-  _mm256_storeu_pd (C+5*ldc, c50_c51_c52_c53);
-  _mm256_storeu_pd (C+6*ldc, c60_c61_c62_c63);
-  _mm256_storeu_pd (C+7*ldc, c70_c71_c72_c73);
+  register __m256d a0x = _mm256_broadcast_sd(A);
+  register __m256d a1x = _mm256_broadcast_sd(A + lda);
+  register __m256d a2x = _mm256_broadcast_sd(A + 2 * lda);
+  register __m256d a3x = _mm256_broadcast_sd(A + 3 * lda);
+  register __m256d a4x = _mm256_broadcast_sd(A + 4 * lda);
+  register __m256d a5x = _mm256_broadcast_sd(A + 5 * lda);
+  register __m256d a6x = _mm256_broadcast_sd(A + 6 * lda);
+  register __m256d a7x = _mm256_broadcast_sd(A + 7 * lda);
+  register __m256d b = _mm256_loadu_pd(B);
 
+  c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
+  c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
+  c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
+  c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
+  c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b, c40_c41_c42_c43);
+  c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
+  c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
+  c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
+  a0x = _mm256_broadcast_sd(A + 1);
+  a1x = _mm256_broadcast_sd(A + 1 + lda);
+  a2x = _mm256_broadcast_sd(A + 1 + 2 * lda);
+  a3x = _mm256_broadcast_sd(A + 1 + 3 * lda);
+  a4x = _mm256_broadcast_sd(A + 1 + 4 * lda);
+  a5x = _mm256_broadcast_sd(A + 1 + 5 * lda);
+  a6x = _mm256_broadcast_sd(A + 1 + 6 * lda);
+  a7x = _mm256_broadcast_sd(A + 1 + 7 * lda);
+  b = _mm256_loadu_pd(B + 1 * ldb);
+  c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
+  c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
+  c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
+  c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
+  c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b, c40_c41_c42_c43);
+  c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
+  c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
+  c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
+  a0x = _mm256_broadcast_sd(A + 2);
+  a1x = _mm256_broadcast_sd(A + 2 + lda);
+  a2x = _mm256_broadcast_sd(A + 2 + 2 * lda);
+  a3x = _mm256_broadcast_sd(A + 2 + 3 * lda);
+  a4x = _mm256_broadcast_sd(A + 2 + 4 * lda);
+  a5x = _mm256_broadcast_sd(A + 2 + 5 * lda);
+  a6x = _mm256_broadcast_sd(A + 2 + 6 * lda);
+  a7x = _mm256_broadcast_sd(A + 2 + 7 * lda);
+  b = _mm256_loadu_pd(B + 2 * ldb);
+  c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
+  c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
+  c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
+  c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
+  c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b, c40_c41_c42_c43);
+  c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
+  c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
+  c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
+  a0x = _mm256_broadcast_sd(A + 3);
+  a1x = _mm256_broadcast_sd(A + 3 + lda);
+  a2x = _mm256_broadcast_sd(A + 3 + 2 * lda);
+  a3x = _mm256_broadcast_sd(A + 3 + 3 * lda);
+  a4x = _mm256_broadcast_sd(A + 3 + 4 * lda);
+  a5x = _mm256_broadcast_sd(A + 3 + 5 * lda);
+  a6x = _mm256_broadcast_sd(A + 3 + 6 * lda);
+  a7x = _mm256_broadcast_sd(A + 3 + 7 * lda);
+  b = _mm256_loadu_pd(B + 3 * ldb);
+  c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
+  c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
+  c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
+  c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
+  c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b, c40_c41_c42_c43);
+  c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b, c50_c51_c52_c53);
+  c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b, c60_c61_c62_c63);
+  c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b, c70_c71_c72_c73);
+#endif
+  _mm256_storeu_pd(C, c00_c01_c02_c03);
+  _mm256_storeu_pd(C + ldc, c10_c11_c12_c13);
+  _mm256_storeu_pd(C + 2 * ldc, c20_c21_c22_c23);
+  _mm256_storeu_pd(C + 3 * ldc, c30_c31_c32_c33);
+  _mm256_storeu_pd(C + 4 * ldc, c40_c41_c42_c43);
+  _mm256_storeu_pd(C + 5 * ldc, c50_c51_c52_c53);
+  _mm256_storeu_pd(C + 6 * ldc, c60_c61_c62_c63);
+  _mm256_storeu_pd(C + 7 * ldc, c70_c71_c72_c73);
 }
 
 /* This auxiliary subroutine performs a smaller dgemm operation
@@ -460,23 +454,22 @@ static inline void do_block5x4(int lda, int ldb, int ldc, int M, int N, int K, d
 #endif
 }
 
-static inline void do_block8x4 (int lda, int ldb, int ldc, int M, int N, int K, double* A, double* B, double* C)
+static inline void do_block8x4(int lda, int ldb, int ldc, int M, int N, int K, double *A, double *B, double *C)
 {
   /* For each row i of A */
-  for (int i = 0; i < M; i+= 8)
+  for (int i = 0; i < M; i += 8)
   {
-    /* For each column j of B */ 
-    for (int j = 0; j < N; j+= 4) 
+    /* For each column j of B */
+    /* Compute C(i,j) */
+    for (int k = 0; k < K; k += 4)
     {
-      /* Compute C(i,j) */
-      for (int k = 0; k < K; k+=4)
+      for (int j = 0; j < N; j += 4)
       {
-        do_block_SIMD8x4(lda, ldb, ldc, A + i*lda + k, B + k*ldb + j, C + i*ldc + j);
+        do_block_SIMD8x4(lda, ldb, ldc, A + i * lda + k, B + k * ldb + j, C + i * ldc + j);
       }
     }
   }
 }
-
 
 static inline void do_blockL1(int lda, int ldb, int ldc, int M, int N, int K, double *A, double *B, double *C)
 {
@@ -539,7 +532,7 @@ static inline void do_blockL2(int lda, int ldb, int ldc, int M, int N, int K, do
 #ifdef ENABLE_L2_CACHING
         do_blockL1(lda, L2_N, ldc, L2_M, L2_N, L2_K, A + i * lda + k, B_L2_CACHED, C + i * ldc + j);
 #else
-        do_blockL1(lda, ldb, ldc, L2_M, L2_N, L2_K, A + i * lda + k, B + j * ldb + k, C + i * ldc + j);
+        do_blockL1(lda, ldb, ldc, L2_M, L2_N, L2_K, A + i * lda + k, B + k * ldb + j, C + i * ldc + j);
 #endif
 #endif
       }
@@ -568,7 +561,6 @@ static double *pad(int lda, int M, int N, double *A)
     {
       newA[i * N + j] = 0;
     }
-    
   }
   return newA;
 }
